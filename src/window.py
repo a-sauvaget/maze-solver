d879547af3c5b1a57180f3b3fbcbb089b9dd6a9d
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-12-19 16:51:10 +0100

    WIP

---
+++ src/window.py
@@ -0,0 +1,9 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    '''Create a new window
+
+    Create a new window with a blank canvas 
+    that the program will then draw on.
+    '''
+    pass
