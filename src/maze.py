fbb4a4063ecc20711b60fb06b4ebc3af887bbe6d
Alexandre Sauvaget <70508923+a-sauvaget@users.noreply.github.com>
2024-12-21 00:35:04 +0100

    ,maze: dessin du labyrinthe de base ok

--- src/maze.py
+++ src/maze.py
@@ -1,3 +1,5 @@
+import time
+
 from window import Window
 from cell import Cell
 
@@ -19,6 +21,9 @@ class Maze():
         '''
         The constructor of the Maze class.
 
+        Attribute : 
+            _cells (list) : The labirynthe's cell list
+
         Parameters :
             x1 (int), y1 (int) : How many pixels from the top and left the maze should start from the side of the window
             num_rows (int) : Number of rows of the maze,
@@ -27,13 +32,14 @@ class Maze():
             cell_size_y (int) : Base point y of the cell,
             win (Window) : Access to the window to draw itself
         '''
-        self.x1 = x1
-        self.y1 = y1
-        self.num_rows = num_rows
-        self.num_cols = num_cols
-        self.cell_size_x = cell_size_x
-        self.cell_size_y = cell_size_y
-        self.win = win
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
 
         self._create_cells()
     
@@ -41,17 +47,39 @@ class Maze():
         '''
         A list of cells to draw (maze shape).
         '''
-        self._cells = []
-
-        for r in range(self.num_rows):
-            rows = []
-            for c in range(self.num_cols):
-                cell = Cell(self.win)
-                rows.append(cell)
-            self._cells.append(rows)
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
+        
             
-    def _draw_cell(self, i, j):
-        pass
+    def _draw_cell(self, i: int, j: int):
+        '''
+        Draw a single cell on the window.
+
+        Parameters :
+            i (int) : Number of the row
+            j (int) : Number of the cell
+        '''
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
 
     def _animate(self):
-        pass
+        '''
+        Visualize the cell being drawn.
+        '''
+        if self._win is None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
